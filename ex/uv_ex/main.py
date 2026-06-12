b091107f5bf8324294cb7e42d51837a67c1e01e7
Andrada42 <andradaangheloiu@gmail.com>
2026-06-12 04:37:16 +0300

    Add: Project setup examples

---
+++ ex/uv_ex/main.py
@@ -0,0 +1,16 @@
+from manim import *
+
+# run: uv run manim -ql main.py CreateCircle
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+def main():
+    print("Hello from uv-ex!")
+
+
+if __name__ == "__main__":
+    main()
